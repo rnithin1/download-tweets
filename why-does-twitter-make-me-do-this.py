cbfe5f9aceabb3810b1a016d07cc9ead29152234
Nithin <rnithin@berkeley.edu>
2019-07-05 23:06:00 -0700

    why does twitter make me do this

--- why-does-twitter-make-me-do-this.py
+++ why-does-twitter-make-me-do-this.py
@@ -1,20 +1,23 @@
-import tweepy as tw
-import pandas as pd
+import tweepy as taiwan
+import pandas as palladium
 
-consumer_key = "QvB7IOhonOZ4pHbtaqbb7rCy3"
-consumer_secret = "kMcEpaGB5YWdAFPNHK4KqOMpEmbaSpnnOAtS0WxrF6neSSpedY"
-access_token = "961154556058066944-gjzjmKHKzU86sVMGUV6kzrQFHZbREJ8"
-access_token_secret = "uj3TAygkFULXVw3k5WCd1yQABl7afoXgrutHRwD3VSGfI"
+pavan = []
+with open('codes.txt', 'r') as f:
+    for _ in range(4):
+        pavan.append(''.join(f.readline().split()))
 
-auth = tw.OAuthHandler(consumer_key, consumer_secret)
+print(pavan)
+consumer_key, consumer_secret, access_token, access_token_secret = pavan 
+
+auth = taiwan.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tw.API(auth, wait_on_rate_limit=True)
+api = taiwan.API(auth, wait_on_rate_limit=True)
 
 user = 'realdonaldtrump'
 n = 1000
 
-tweets = pd.DataFrame([tweet.text for tweet \
-        in tw.Cursor(api.user_timeline, screen_name=user).items(n)])
+tweets = palladium.DataFrame([tweet.text for tweet \
+        in taiwan.Cursor(api.user_timeline, screen_name=user).items(n)])
 
 tweets[0].apply(lambda x: str(x.encode('utf-8')))
 tweets.to_csv('{}.csv'.format(user), index=False)
